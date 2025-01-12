ee8a1ca5cbc464ecabf1e6f064b6c9cc6cbefaa4
mashoun <mashoun.me@gmail.com>
2025-01-12 20:49:26 +0200

    fix: remove unnecessary class from service section title for improved styling

--- src/components/services-section.vue
+++ src/components/services-section.vue
@@ -11,7 +11,7 @@
                             <div class="sb-icon ratio ratio-1x1" style="width: 150px;">
                                 <img :src="node.icon" alt="icon" class="object-fit-cover">
                             </div>
-                            <strong class="sb-title text-center fs-5 font-monti text-igc-100">{{ node.title }}</strong>
+                            <strong class="sb-title text-center fs-5 font-monti">{{ node.title }}</strong>
                             <p class="sb-description text-igc-light font-inter text-center">{{ node.description }}</p>
                         </section>
                     </a>
